e739bbbf3d1ad26ff274c931bbb812241e926284
sojedaa <98702956+sojedaa@users.noreply.github.com>
2022-10-09 23:29:05 -0500

    Update utils.py

--- Part1/utils.py
+++ Part1/utils.py
@@ -7,8 +7,6 @@ import numpy as np
 def make_beta_schedule(schedule='linear', n_timesteps=1000, start=1e-5, end=1e-2):
     if schedule == 'linear':
         betas = torch.linspace(start, end, n_timesteps)
-    elif schedule == "quad":
-        betas = torch.linspace(start ** 0.5, end ** 0.5, n_timesteps) ** 2
     elif schedule == "sigmoid":
         betas = torch.linspace(-6, 6, n_timesteps)
         betas = torch.sigmoid(betas) * (end - start) + start
